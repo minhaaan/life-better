143d1e5c991d02520a33ed6db317ef5a0cd527c6
최민한 <chmh0411@gmail.com>
2022-11-20 22:31:27 +0900

    refact: app target, app test target 만드는 함수 맞춤화

--- Targets/LifeBetter/Project.swift
+++ Targets/LifeBetter/Project.swift
@@ -21,46 +21,10 @@ import ProjectDescriptionHelpers
 
 // Creates our project using a helper function defined in ProjectDescriptionHelpers
 
-let infoPlist: [String: InfoPlist.Value] = [
-  "CFBundleShortVersionString": "1.0",
-  "CFBundleVersion": "1",
-  "UIMainStoryboardFile": "",
-  "UILaunchStoryboardName": "LaunchScreen"
-]
-
-let appTarget = Target(
-  name: "LifeBetter",
-  platform: .iOS,
-  product: .app,
-  productName: "LifeBetter",
-  bundleId: "com.minan.LifeBetter",
-  deploymentTarget: .iOS(targetVersion: "16.1", devices: [.iphone]),
-  infoPlist: .extendingDefault(with: infoPlist),
-  sources: ["Sources/**"],
-  resources: ["Resources/**"],
-  scripts: [],
-  dependencies: [],
-  settings: nil,
-  environment: [:]
-)
-
-let testTarget = Target(
-  name: "LifeBetterTests",
-  platform: .iOS,
-  product: .unitTests,
-  productName: "LifeBetterTests",
-  bundleId: "com.minan.LifeBetterTests",
-  deploymentTarget: .iOS(targetVersion: "16.1", devices: [.iphone]),
-  infoPlist: .default,
-  sources: ["Tests/**"],
-  dependencies: [],
-  settings: nil
-)
-
 let project = Project(
   name: "LifeBetter",
   organizationName: "com.minan",
   packages: [],
   settings: nil,
-  targets: [appTarget, testTarget]
+  targets: Project.makeAppTargets(name: "LifeBetter", dependencies: [])
 )
