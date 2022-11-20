7e399798517be4944eab2459b45ce89d2d306edc
최민한 <chmh0411@gmail.com>
2022-11-20 22:43:30 +0900

    add: SnapKit, ModernRibs, CombineExt

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -4,6 +4,8 @@ import ProjectDescriptionHelpers
 let dependencies = Dependencies(
   carthage: nil,
   swiftPackageManager: SwiftPackageManagerDependencies([
-    
+    .remote(url: "https://github.com/SnapKit/SnapKit", requirement: .exact("5.6.0")),
+    .remote(url: "https://github.com/DevYeom/ModernRIBs", requirement: .exact("1.0.2")),
+    .remote(url: "https://github.com/CombineCommunity/CombineExt", requirement: .exact("1.8.1")),
   ]),
   platforms: [.iOS])
