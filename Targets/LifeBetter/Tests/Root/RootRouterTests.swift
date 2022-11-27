64693193c2d66ad5320c400eace1b5c99c51a79b
최민한 <chmh0411@gmail.com>
2022-11-27 16:29:59 +0900

    add: Root Test Files

---
+++ Targets/LifeBetter/Tests/Root/RootRouterTests.swift
@@ -0,0 +1,30 @@
+//
+//  RootRouterTests.swift
+//  LifeBetterTests
+//
+//  Created by 최민한 on 2022/11/27.
+//  Copyright © 2022 com.minan. All rights reserved.
+//
+
+@testable import LifeBetter
+import XCTest
+
+final class RootRouterTests: XCTestCase {
+  
+  private var router: RootRouter!
+  
+  // TODO: declare other objects and mocks you need as private vars
+  
+  override func setUp() {
+    super.setUp()
+    
+    // TODO: instantiate objects and mocks
+  }
+  
+  // MARK: - Tests
+  
+  func test_routeToExample_invokesToExampleResult() {
+    // This is an example of a router test case.
+    // Test your router functions invokes the corresponding builder, attachesChild, presents VC, etc.
+  }
+}
