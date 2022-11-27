929b3ac3e86617549cbf64cf76533716d1130c99
최민한 <chmh0411@gmail.com>
2022-11-28 03:03:15 +0900

    imple: SubwayListViewController temporary ui

--- Targets/Subway/Subway/Sources/List/SubwayListViewController.swift
+++ Targets/Subway/Subway/Sources/List/SubwayListViewController.swift
@@ -10,12 +10,39 @@ import ModernRIBs
 import UIKit
 
 protocol SubwayListPresentableListener: AnyObject {
-    // TODO: Declare properties and methods that the view controller can invoke to perform
-    // business logic, such as signIn(). This protocol is implemented by the corresponding
-    // interactor class.
+  // TODO: Declare properties and methods that the view controller can invoke to perform
+  // business logic, such as signIn(). This protocol is implemented by the corresponding
+  // interactor class.
 }
 
 final class SubwayListViewController: UIViewController, SubwayListPresentable, SubwayListViewControllable {
-
-    weak var listener: SubwayListPresentableListener?
+  
+  weak var listener: SubwayListPresentableListener?
+  
+  private let subwayStations: [SubwayStation]
+  
+  // MARK: init
+  
+  init(subwayStations: [SubwayStation]) {
+    self.subwayStations = subwayStations
+    super.init(nibName: nil, bundle: nil)
+  }
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  // MARK: LifeCycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupLayout()
+  }
+  
+  // MARK: func
+  
+  private func setupLayout() {
+    view.backgroundColor = .systemPurple
+  }
+  
 }
