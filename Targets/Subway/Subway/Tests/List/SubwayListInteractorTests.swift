e8e390b6566beea40de71f3dc539da7f6b20b5c6
최민한 <chmh0411@gmail.com>
2022-11-28 03:13:06 +0900

    add: SubwayList Test Files

---
+++ Targets/Subway/Subway/Tests/List/SubwayListInteractorTests.swift
@@ -0,0 +1,30 @@
+//
+//  SubwayListInteractorTests.swift
+//  Subway
+//
+//  Created by 최민한 on 2022/11/28.
+//  Copyright © 2022 com.minan. All rights reserved.
+//
+
+@testable import Subway
+import XCTest
+
+final class SubwayListInteractorTests: XCTestCase {
+  
+  private var interactor: SubwayListInteractor!
+  
+  // TODO: declare other objects and mocks you need as private vars
+  
+  override func setUp() {
+    super.setUp()
+    
+    // TODO: instantiate objects and mocks
+  }
+  
+  // MARK: - Tests
+  
+  func test_exampleObservable_callsRouterOrListener_exampleProtocol() {
+    // This is an example of an interactor test case.
+    // Test your interactor binds observables and sends messages to router or listener.
+  }
+}
---
+++ Targets/Subway/Subway/Tests/List/SubwayListRouterTests.swift
@@ -0,0 +1,30 @@
+//
+//  SubwayListRouterTests.swift
+//  Subway
+//
+//  Created by 최민한 on 2022/11/28.
+//  Copyright © 2022 com.minan. All rights reserved.
+//
+
+@testable import Subway
+import XCTest
+
+final class SubwayListRouterTests: XCTestCase {
+  
+  private var router: SubwayListRouter!
+  
+  // TODO: declare other objects and mocks you need as private vars
+  
+  override func setUp() {
+    super.setUp()
+    
+    // TODO: instantiate objects and mocks
+  }
+  
+  // MARK: - Tests
+  
+  func test_routeToExample_invokesToExampleResult() {
+    // This is an example of a router test case.
+    // Test your router functions invokes the corresponding builder, attachesChild, presents VC, etc.
+  }
+}
