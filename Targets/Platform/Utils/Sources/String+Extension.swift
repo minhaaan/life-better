9c90b99b4140e0dd7a3485d8ee5e10a1fe993443
최민한 <chmh0411@gmail.com>
2022-11-30 18:15:39 +0900

    add: String Extension isNotEmpty

---
+++ Targets/Platform/Utils/Sources/String+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  String+Extension.swift
+//  Utils
+//
+//  Created by 최민한 on 2022/11/30.
+//  Copyright © 2022 com.minan. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+  var isNotEmpty: Bool {
+    return !self.isEmpty
+  }
+}
