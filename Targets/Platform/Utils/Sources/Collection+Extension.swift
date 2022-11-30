78a6184c26e14618ad4bc0c9fc68aa256c23d9b0
최민한 <chmh0411@gmail.com>
2022-11-30 18:10:34 +0900

    add: Collection Extension isNotEmpty 추가

---
+++ Targets/Platform/Utils/Sources/Collection+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Collection+Extension.swift
+//  Utils
+//
+//  Created by 최민한 on 2022/11/30.
+//  Copyright © 2022 com.minan. All rights reserved.
+//
+
+import Foundation
+
+public extension Collection {
+  var isNotEmpty: Bool {
+    return !self.isEmpty
+  }
+}
