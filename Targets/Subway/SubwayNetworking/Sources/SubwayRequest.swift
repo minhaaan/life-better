635be0c4d221225b3d826936d27994d02f82adc0
최민한 <chmh0411@gmail.com>
2022-12-02 00:00:41 +0900

    Create SubwayRequest.swift

---
+++ Targets/Subway/SubwayNetworking/Sources/SubwayRequest.swift
@@ -0,0 +1,24 @@
+import Foundation
+import Networking
+
+public struct SubwayRequest {
+  let stationName: String
+}
+
+extension SubwayRequest: RequestProtocol {
+  public var host: String {
+    "http://swopenAPI.seoul.go.kr/api/subway/(인증키)/json/realtimeStationArrival/0/5"
+  }
+  
+  public var path: String {
+    return "/\(self.stationName)"
+  }
+  
+  public var requestType: Networking.RequestType {
+    .GET
+  }
+  
+  public var header: [String : String] {
+    [:]
+  }
+}
