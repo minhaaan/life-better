cdae88cb3aa98bea878d88f38ee7bc94186bc96d
최민한 <chmh0411@gmail.com>
2022-12-02 20:06:57 +0900

    impl: SubwayListRouter Tests

--- Targets/Subway/Subway/Tests/List/SubwayListRouterTests.swift
+++ Targets/Subway/Subway/Tests/List/SubwayListRouterTests.swift
@@ -8,23 +8,60 @@
 
 @testable import Subway
 import XCTest
+import SubwayDetail
 
 final class SubwayListRouterTests: XCTestCase {
   
   private var router: SubwayListRouter!
+  private var subwayListInteractor: SubwayListInteractableMock!
+  private var subwayListViewControllable: SubwayListViewControllableMock!
+  private var subwayDetailBuildable: SubwayDetailBuildableMock!
+  private var subwayDetailListner: SubwayDetailListener!
+  private var subwayDetailRouting: SubwayDetailRoutingMock!
   
   // TODO: declare other objects and mocks you need as private vars
   
   override func setUp() {
     super.setUp()
     
+    /*
+     interactor, viewController, detailBuilderMock 필요
+     */
+    
+    self.subwayListInteractor = SubwayListInteractableMock()
+    self.subwayListViewControllable = SubwayListViewControllableMock()
+    self.subwayDetailBuildable = SubwayDetailBuildableMock()
+    self.subwayDetailListner = SubwayDetailListnerMock()
+    self.subwayDetailRouting = SubwayDetailRoutingMock(
+      viewControllable: SubwayListViewControllableMock(),
+      interactable: subwayListInteractor
+    )
+    
+    self.subwayDetailBuildable.buildHandler = { (listner: SubwayDetailListener) -> SubwayDetailRouting in
+      self.subwayDetailListner = listner
+      return self.subwayDetailRouting
+    }
+    
     // TODO: instantiate objects and mocks
+    self.router = SubwayListRouter(
+      interactor: subwayListInteractor,
+      viewController: subwayListViewControllable,
+      subwayDetailBuilder: subwayDetailBuildable
+    )
   }
   
   // MARK: - Tests
-  
-  func test_routeToExample_invokesToExampleResult() {
-    // This is an example of a router test case.
-    // Test your router functions invokes the corresponding builder, attachesChild, presents VC, etc.
+ 
+  func test_attachSubwayDetail() {
+    // GIVEN
+    let stationName: String = "죽전"
+    
+    // WHEN
+    router.attachSubwayDetail(stationName: stationName)
+    
+    // THEN
+    XCTAssert(subwayDetailBuildable.buildCallsCount == 1)
+    XCTAssert(subwayDetailRouting.loadCalled == true)
   }
+  
 }
