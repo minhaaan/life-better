e9e20855d9226a254b63fab0ac93e9221f4c94e9
최민한 <chmh0411@gmail.com>
2022-12-06 06:25:55 +0900

    refact: graph에 SubwayDemoApp 안나오게 설정

--- Workspace.swift
+++ Workspace.swift
@@ -7,6 +7,7 @@ let workspace = Workspace(
     "Targets/Subway/*",
     "Targets/Platform/*",
     "Targets/Core/*",
-    "Targets/Core/Networking/*"
+//    "Targets/Core/Networking/*", // Networking Demo app
+//    "Targets/Subway/Subway/*" // Subway Demo app
   ]
 )
