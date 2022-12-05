b18a9cb19d81e94cbd06d36a764a7cace713408e
최민한 <chmh0411@gmail.com>
2022-12-06 06:00:41 +0900

    feat: add SwiftyBeaber Cloud Key in Keys

--- Targets/Platform/Utils/Sources/Keys.swift
+++ Targets/Platform/Utils/Sources/Keys.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 public struct Keys {
-public static let SUBWAY_API_KEY = ""
+  public static let SUBWAY_API_KEY = ""
+  public static let SWIFTY_BEAVER_KEY = SwiftyBeaverKeys()
+}
+
+public struct SwiftyBeaverKeys {
+  public let appId = ""
+  public let secret = ""
+  public let encryptionKey = ""
 }
