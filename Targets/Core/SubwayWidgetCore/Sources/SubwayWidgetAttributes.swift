0192d27069d5a911da124c1ea2a9c86c8bb9c3d4
최민한 <chmh0411@gmail.com>
2022-12-07 20:36:57 +0900

    feat: SubwayWidgetAttributes ContentState public init

--- Targets/Core/SubwayWidgetCore/Sources/SubwayWidgetAttributes.swift
+++ Targets/Core/SubwayWidgetCore/Sources/SubwayWidgetAttributes.swift
@@ -4,7 +4,11 @@ import ActivityKit
 public struct SubwayWidgetAttributes: ActivityAttributes {
   public struct ContentState: Codable, Hashable {
       // Dynamic stateful properties about your activity go here!
-      var value: Int
+    var value: Int
+    
+    public init(value: Int) {
+      self.value = value
+    }
   }
 
   // Fixed non-changing properties about your activity go here!
