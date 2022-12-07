37a1ffe05ac4cfb45fc63260dd6b7391edcedc30
최민한 <chmh0411@gmail.com>
2022-12-07 23:55:03 +0900

    refact: indent arrangement

--- Targets/LifeBetter/SubwayWidget/Sources/SubwayWidgetLiveActivity.swift
+++ Targets/LifeBetter/SubwayWidget/Sources/SubwayWidgetLiveActivity.swift
@@ -11,38 +11,38 @@ import SwiftUI
 import SubwayWidgetCore
 
 struct SubwayWidgetLiveActivity: Widget {
-    var body: some WidgetConfiguration {
-        ActivityConfiguration(for: SubwayWidgetAttributes.self) { context in
-            // Lock screen/banner UI goes here
-            VStack {
-                Text("Hello")
-            }
-            .activityBackgroundTint(Color.cyan)
-            .activitySystemActionForegroundColor(Color.black)
-            
-        } dynamicIsland: { context in
-            DynamicIsland {
-                // Expanded UI goes here.  Compose the expanded UI through
-                // various regions, like leading/trailing/center/bottom
-                DynamicIslandExpandedRegion(.leading) {
-                    Text("Leading")
-                }
-                DynamicIslandExpandedRegion(.trailing) {
-                    Text("Trailing")
-                }
-                DynamicIslandExpandedRegion(.bottom) {
-                    Text("Bottom")
-                    // more content
-                }
-            } compactLeading: {
-                Text("L")
-            } compactTrailing: {
-                Text("T")
-            } minimal: {
-                Text("Min")
-            }
-            .widgetURL(URL(string: "http://www.apple.com"))
-            .keylineTint(Color.red)
+  var body: some WidgetConfiguration {
+    ActivityConfiguration(for: SubwayWidgetAttributes.self) { context in
+      // Lock screen/banner UI goes here
+      VStack {
+        Text("Hello")
+      }
+      .activityBackgroundTint(Color.cyan)
+      .activitySystemActionForegroundColor(Color.black)
+      
+    } dynamicIsland: { context in
+      DynamicIsland {
+        // Expanded UI goes here.  Compose the expanded UI through
+        // various regions, like leading/trailing/center/bottom
+        DynamicIslandExpandedRegion(.leading) {
+          Text("Leading")
         }
+        DynamicIslandExpandedRegion(.trailing) {
+          Text("Trailing")
+        }
+        DynamicIslandExpandedRegion(.bottom) {
+          Text("Bottom")
+          // more content
+        }
+      } compactLeading: {
+        Text("L")
+      } compactTrailing: {
+        Text("T")
+      } minimal: {
+        Text("Min")
+      }
+      .widgetURL(URL(string: "http://www.apple.com"))
+      .keylineTint(Color.red)
     }
+  }
 }
