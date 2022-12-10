55b9204cd18dfe0222ce5ef16b28f2521df40de4
최민한 <chmh0411@gmail.com>
2022-12-10 21:39:58 +0900

    feat: RootListViewController Content array 생성자로 주입

--- Targets/LifeBetter/Sources/Root/List/RootListBuilder.swift
+++ Targets/LifeBetter/Sources/Root/List/RootListBuilder.swift
@@ -9,32 +9,34 @@
 import ModernRIBs
 
 protocol RootListDependency: Dependency {
-    // TODO: Declare the set of dependencies required by this RIB, but cannot be
-    // created by this RIB.
+  // TODO: Declare the set of dependencies required by this RIB, but cannot be
+  // created by this RIB.
+  var content: [Content] { get }
 }
 
 final class RootListComponent: Component<RootListDependency> {
-
-    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+  fileprivate var contents: [Content] {
+    dependency.content
+  }
 }
 
 // MARK: - Builder
 
 protocol RootListBuildable: Buildable {
-    func build(withListener listener: RootListListener) -> RootListRouting
+  func build(withListener listener: RootListListener) -> RootListRouting
 }
 
 final class RootListBuilder: Builder<RootListDependency>, RootListBuildable {
-
-    override init(dependency: RootListDependency) {
-        super.init(dependency: dependency)
-    }
-
-    func build(withListener listener: RootListListener) -> RootListRouting {
-        let component = RootListComponent(dependency: dependency)
-        let viewController = RootListViewController()
-        let interactor = RootListInteractor(presenter: viewController)
-        interactor.listener = listener
-        return RootListRouter(interactor: interactor, viewController: viewController)
-    }
+  
+  override init(dependency: RootListDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  func build(withListener listener: RootListListener) -> RootListRouting {
+    let component = RootListComponent(dependency: dependency)
+    let viewController = RootListViewController(contents: component.contents)
+    let interactor = RootListInteractor(presenter: viewController)
+    interactor.listener = listener
+    return RootListRouter(interactor: interactor, viewController: viewController)
+  }
 }
--- Targets/LifeBetter/Sources/Root/List/RootListViewController.swift
+++ Targets/LifeBetter/Sources/Root/List/RootListViewController.swift
@@ -10,19 +10,38 @@ import ModernRIBs
 import UIKit
 
 protocol RootListPresentableListener: AnyObject {
-    // TODO: Declare properties and methods that the view controller can invoke to perform
-    // business logic, such as signIn(). This protocol is implemented by the corresponding
-    // interactor class.
+  // TODO: Declare properties and methods that the view controller can invoke to perform
+  // business logic, such as signIn(). This protocol is implemented by the corresponding
+  // interactor class.
 }
 
 final class RootListViewController: UIViewController, RootListPresentable, RootListViewControllable {
-
-    weak var listener: RootListPresentableListener?
   
+  weak var listener: RootListPresentableListener?
+  
+  // MARK: Properties
+  
+  let contents: [Content]
+  
+  // MARK: init
+  
+  init(contents: [Content]) {
+    self.contents = contents
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  // MARK: LifeCycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     view.backgroundColor = .systemPink
   }
+  
+  // MARK: Method
+  
 }
