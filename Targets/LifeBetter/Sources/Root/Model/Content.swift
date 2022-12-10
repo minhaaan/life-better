1e3062eaeb3d18f939e9e449b28d5e41d969a4eb
최민한 <chmh0411@gmail.com>
2022-12-10 21:02:41 +0900

    feat: add: Root List Content struct

---
+++ Targets/LifeBetter/Sources/Root/Model/Content.swift
@@ -0,0 +1,14 @@
+//
+//  Content.swift
+//  LifeBetter
+//
+//  Created by 최민한 on 2022/12/10.
+//  Copyright © 2022 com.minan. All rights reserved.
+//
+
+import Foundation
+
+struct Content: Hashable {
+  let imageName: String
+  let name: String
+}
